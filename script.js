b04a5ae7bb93d27977712e02ba4bdd15a5f1d876
Aayam <akc6@emory.edu>
2023-09-27 16:09:02 -0400

    Demo for HTML/CSS/JS

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+// Select the div element by its id
+const interactiveBox = document.getElementById("interactive-box");
+
+// Add a click event listener
+interactiveBox.addEventListener("click", function () {
+  // Display a prompt when the div is clicked
+  alert("You have clicked the button!");
+});
+// Your JavaScript code here
+});
