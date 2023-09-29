5f9b7f28f055a4457c0e251be772d287c1659440
Frederic Guintu <frederic.guintu@emory.edu>
2023-09-28 20:06:35 -0400

    css/html/java practice files 9-28

---
+++ htmlpractice/demo.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function (){
+const interact = document.getElementById("interact");
+
+interact.addEventListener("click", function (){
+    alert("Yayayay you clicked my button");
+});
+});
